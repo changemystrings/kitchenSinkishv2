e37106bb58b7fbe906c35c4e372fd410cccd4158
tucker <michael.p.tucker@gmail.com>
2015-02-04 21:43:54 -0500

    Working api data object

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,17 +1,12 @@
 module.exports = function (app,passport,apiData) {
   var User = require('../../models/user');
-
-//POST create new user
-  app.get('/users/create', function (req, res) {
-    var user = new User();
-    user.name = 'user2';
-    user.save();
-    res.send('respond with a resource');
-  });
+  var security = require('../../utility/secureRoute');
 
 //GET existing users
   app.get('/users', function (req, res) {
-    var sess = req.session;
+    var secFunction = security();
+      var apiObj = secFunction(req,'rolename',apiData);
+    var api = apiData;
       User.find(function (err, users) {
         if (err)
           res.send(err);
@@ -19,18 +14,35 @@ module.exports = function (app,passport,apiData) {
       });
   });
 
+  //function secureLogin(req,roleName,apiData) {
+  //  apiData.requestRequiresAuthentication = true;
+  //  if (apiData) {
+  //    apiData.userIsAuthenticated = req.isAuthenticated();
+  //    if (roleName) {
+  //      apiData.requestRequiresAuthorization = true;
+  //      apiData.requiredRoleForRequest = roleName;
+  //      if (req.isAuthenticated()) {
+  //        if (apiData.currentUser.roles && apiData.currentUser.roles.length > 0) {
+  //          if (apiData.currentUser.roles.indexOf(roleName) >= 0) {
+  //            apiData.requestIsAuthorized = true;
+  //            apiData.userHasRequiredRole = true;
+  //          }
+  //        }
+  //      }
+  //      else {
+  //        apiData.requestIsAuthorized = true;
+  //      }
+  //    }
+  //    else {
+  //      apiData.requestIsAuthorized = req.isAuthenticated();
+  //    }
+  //  }
+  //  return apiData;
+  //}
+
 //Get existing single user by ID
   app.get('users/:userId', function (req, res) {
     User.findById(req.params.userId, function (err, user) {
-      if (err)
-        res.send(err);
-      res.json(user);
-    });
-  });
-
-//GET existing single user by Name
-  app.get('users/name/:userName', function (req, res) {
-    User.findOne({'name': req.params.userName}, function (err, user) {
       if (err)
         res.send(err);
       res.json(user);
